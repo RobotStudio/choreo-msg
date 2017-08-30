66d0ddf0d2009abe515ca2d36b70020a352ddb18
Bobby <karma0@gmail.com>
2017-08-30 17:10:18 -0600

    Init with primitives

---
+++ generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+builds="build"
+
+PROTOC="$(which protoc)"
+[ -f "$PROTOC" ] || { echo "Do you have protoc installed?"; exit 1; }
+
+lang_opts=
+[ "$#" -gt 0 ] || lang_opts="--go_out=go"
+for lang in $*; do
+  lang_opts="$lang_opts --${lang}_out=$lang"
+  [ -d "$builds/$lang" ] || mkdir -p "$builds/$lang"
+done
+
+find . -iname '*.proto' -type f | while read -r f; do
+  p="$(dirname "$f")"
+  protoc -I="$p" $lang_opts "$f"
+done
+
+exit 0
